7ac6cd1f7a4ddf76bf858678e8581ccdf722d9fc
sunjx <raymooonn@gmail.com>
2024-01-02 23:30:29 +0800

    md5

--- src/encode/fileEncode.ts
+++ src/encode/fileEncode.ts
@@ -1,83 +1,115 @@
 import { Injectable } from "@nestjs/common";
 
-const splitFile = require("split-file");
-const CryptoJS = require("crypto-js");
 const crypto = require("crypto");
 
-
-const key = Buffer.from('0123456789abcdef', 'utf8'); // 128位的密钥
-const iv = Buffer.from('abcdef9876543210', 'utf8'); // 128位的初始向量
+const key = Buffer.from("1234567890123456", "binary"); // 128位的密钥
+const iv = Buffer.from("1234567890123456", "binary"); // 128位的密钥
 
 const fs = require("fs");
 
-const option = {
-  iv: iv,
-  mode: CryptoJS.mode.CBC,
-  padding: CryptoJS.pad.Pkcs7,
-  keySize: 128 / 8
-};
 
-const inputPath = "Z:/data/encode/input.mp4";
-const outputPath = "Z:/data/encode/output.tmp";
-const checkPath = "Z:/data/encode/check.mp4";
+const inputPath = "Z:/data/encode/input.pdf";
+const outputPath = "Z:/data/encode/output.mp4";
+const checkPath = "Z:/data/encode/check.pdf";
 const chunkSize = 1024 * 1024 * 2;
-const deSize = 2097168;
+const deSize = chunkSize;
+// const deSize = 2097168;
+
 @Injectable()
 export class FileEncodeUtil {
 
 // 加密函数
   encryptData(data) {
-    const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
-    return Buffer.concat([cipher.update(data), cipher.final()]);
+    const cipher = crypto.createCipheriv("aes-128-cbc", key, iv);
+    cipher.setAutoPadding(true)
+    let result = cipher.update(data, "binary", "binary");
+    result += cipher.final("binary");
+    return result
+    // return Buffer.from(result, "binary");
+    // return Buffer.concat([cipher.update(data),cipher.final()])
   }
 
 // 解密函数
   decryptData(data) {
-    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-    return Buffer.concat([decipher.update(data), decipher.final()]);
+    const decipher = crypto.createDecipheriv("aes-128-cbc", key, iv);
+    decipher.setAutoPadding(true)
+    let result = decipher.update(data, "binary", "binary");
+    result += decipher.final("binary");
+    return result;
+    // return decipher.update(data)
   }
 
   fileSplitEncode() {
-    console.log(key.length);
-    splitFile.splitFileBySize(inputPath, chunkSize)
-      .then((chunks) => {
-        for (let i = 0; i < chunks.length; i++) {
-          const chunkData = fs.readFileSync(chunks[i],null); // 读取切片文件内容
-          const encryptedChunk = this.encryptData(chunkData); // 加密切片内容
-          console.log("完成  " + i + '长度' + encryptedChunk.length);
-          // const outputFileName = `${outputDir}/chunk_${i}.enc`; // 生成输出文件名
-          fs.appendFile(outputPath, encryptedChunk,null,(e)=>{
-            fs.unlink(chunks[i], (err) => {
-              if (err) throw err;
-              // console.log(`Chunk ${idx + 1} deleted`);
-            });
-          }); // 写入加密后的切片文件
-        }
-        console.log('文件加密完成');
-      });
+    fs.unlink(outputPath, () => {
+    });
+    let chunkIndex = 0;
+    const readStream = fs.createReadStream(inputPath, { highWaterMark: chunkSize});
+    readStream.on("data", (chunk) => {
+      console.log("原始" + chunk.length);
+      const encryptedChunk = this.encryptData(chunk); // 加密切片内容
+      console.log("完成  " + chunkIndex + "长度" + encryptedChunk.length);
+      fs.appendFileSync(outputPath, encryptedChunk, { encoding: "binary" });
+      chunkIndex++;
+    });
+
+    readStream.on("end", () => {
+      console.log("文件分片写入完成");
+    });
+
   }
 
 
   fileSplitDecode() {
-    splitFile.splitFileBySize(outputPath, deSize)
-      .then(chunks=>{
-        console.log(chunks.length);
-        for (let i = 0; i < chunks.length; i++) {
-          const encryptedChunk = fs.readFileSync(chunks[i], null); // 读取切片文件内容
-          // const encryptedChunk = chunks[i]; // 读取切片文件内容
-          const decryptedChunk = this.decryptData(encryptedChunk); // 解密切片内容
-          console.log('完成' + i);
-          fs.appendFile(checkPath, decryptedChunk,null,(e)=>{
-            fs.unlink(chunks[i], (err) => {
-              if (err) throw err;
-              // console.log(`Chunk ${idx + 1} deleted`);
-            });
-          });
-        }
-        console.log('文件解密完成');
-      })
+    fs.unlink(checkPath, () => {
+    });
+    let chunkIndex = 0;
+    const readStream = fs.createReadStream(outputPath, { highWaterMark: deSize });
+    readStream.on("data", (chunk) => {
+      console.log("原始" + chunk.length);
+      const deChunk = this.decryptData(chunk); // 加密切片内容
+      console.log("完成  " + chunkIndex + "长度" + deChunk.length);
+      fs.appendFileSync(checkPath, deChunk, { encoding: "binary" });
+      chunkIndex++;
+    });
+
+    readStream.on("end", () => {
+      console.log("文件解密完成");
+      let result = this.fileCheck();
+    });
 
 
+  }
+
+
+  // 计算文件的 MD5 散列值
+  calculateMD5(filePath) {
+    return new Promise((resolve, reject) => {
+      const hash = crypto.createHash("md5");
+      const readStream = fs.createReadStream(filePath, { encoding: "binary" });
+
+      readStream.on("data", (chunk) => {
+        hash.update(chunk);
+      });
+
+      readStream.on("end", () => {
+        const md5Hash = hash.digest("hex");
+        resolve(md5Hash);
+      });
+
+      readStream.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+
+  fileCheck() {
+    this.calculateMD5(inputPath).then(r => {
+      console.log("原始" + r);
+    });
+    this.calculateMD5(checkPath).then(r => {
+      console.log("目标" + r);
+    });
   }
 
 
